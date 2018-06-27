782b9a6bc009c28ede4e4907cb6152c992f154e0
alpaca <36502013+Alpaca-Tech@users.noreply.github.com>
2018-06-27 20:24:22 +0800

    Add files via upload

---
+++ 7_segment_display/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace _7_segment_display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_1 = new System.Windows.Forms.Button();
+            this.bt_2 = new System.Windows.Forms.Button();
+            this.bt_3 = new System.Windows.Forms.Button();
+            this.bt_4 = new System.Windows.Forms.Button();
+            this.bt_5 = new System.Windows.Forms.Button();
+            this.bt_6 = new System.Windows.Forms.Button();
+            this.bt_7 = new System.Windows.Forms.Button();
+            this.bt_rand = new System.Windows.Forms.Button();
+            this.bt_sc = new System.Windows.Forms.Button();
+            this.lb_show = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_1
+            // 
+            this.bt_1.Location = new System.Drawing.Point(41, 13);
+            this.bt_1.Name = "bt_1";
+            this.bt_1.Size = new System.Drawing.Size(175, 28);
+            this.bt_1.TabIndex = 0;
+            this.bt_1.Text = "button1";
+            this.bt_1.UseVisualStyleBackColor = true;
+            this.bt_1.Click += new System.EventHandler(this.bt_1_Click);
+            // 
+            // bt_2
+            // 
+            this.bt_2.Location = new System.Drawing.Point(213, 47);
+            this.bt_2.Name = "bt_2";
+            this.bt_2.Size = new System.Drawing.Size(31, 151);
+            this.bt_2.TabIndex = 1;
+            this.bt_2.Text = "button1";
+            this.bt_2.UseVisualStyleBackColor = true;
+            this.bt_2.Click += new System.EventHandler(this.bt_2_Click);
+            // 
+            // bt_3
+            // 
+            this.bt_3.Location = new System.Drawing.Point(213, 238);
+            this.bt_3.Name = "bt_3";
+            this.bt_3.Size = new System.Drawing.Size(31, 151);
+            this.bt_3.TabIndex = 2;
+            this.bt_3.Text = "button2";
+            this.bt_3.UseVisualStyleBackColor = true;
+            this.bt_3.Click += new System.EventHandler(this.bt_3_Click);
+            // 
+            // bt_4
+            // 
+            this.bt_4.Location = new System.Drawing.Point(41, 395);
+            this.bt_4.Name = "bt_4";
+            this.bt_4.Size = new System.Drawing.Size(175, 28);
+            this.bt_4.TabIndex = 3;
+            this.bt_4.Text = "button1";
+            this.bt_4.UseVisualStyleBackColor = true;
+            this.bt_4.Click += new System.EventHandler(this.bt_4_Click);
+            // 
+            // bt_5
+            // 
+            this.bt_5.Location = new System.Drawing.Point(12, 238);
+            this.bt_5.Name = "bt_5";
+            this.bt_5.Size = new System.Drawing.Size(31, 151);
+            this.bt_5.TabIndex = 4;
+            this.bt_5.Text = "button4";
+            this.bt_5.UseVisualStyleBackColor = true;
+            this.bt_5.Click += new System.EventHandler(this.bt_5_Click);
+            // 
+            // bt_6
+            // 
+            this.bt_6.Location = new System.Drawing.Point(12, 47);
+            this.bt_6.Name = "bt_6";
+            this.bt_6.Size = new System.Drawing.Size(31, 151);
+            this.bt_6.TabIndex = 5;
+            this.bt_6.Text = "button5";
+            this.bt_6.UseVisualStyleBackColor = true;
+            this.bt_6.Click += new System.EventHandler(this.bt_6_Click);
+            // 
+            // bt_7
+            // 
+            this.bt_7.Location = new System.Drawing.Point(41, 204);
+            this.bt_7.Name = "bt_7";
+            this.bt_7.Size = new System.Drawing.Size(175, 28);
+            this.bt_7.TabIndex = 6;
+            this.bt_7.Text = "button1";
+            this.bt_7.UseVisualStyleBackColor = true;
+            this.bt_7.Click += new System.EventHandler(this.bt_7_Click);
+            // 
+            // bt_rand
+            // 
+            this.bt_rand.Font = new System.Drawing.Font("新細明體", 20F);
+            this.bt_rand.Location = new System.Drawing.Point(271, 395);
+            this.bt_rand.Name = "bt_rand";
+            this.bt_rand.Size = new System.Drawing.Size(159, 38);
+            this.bt_rand.TabIndex = 7;
+            this.bt_rand.Text = "隨機產生";
+            this.bt_rand.UseVisualStyleBackColor = true;
+            this.bt_rand.Click += new System.EventHandler(this.bt_rand_Click);
+            // 
+            // bt_sc
+            // 
+            this.bt_sc.Font = new System.Drawing.Font("新細明體", 20F);
+            this.bt_sc.Location = new System.Drawing.Point(271, 438);
+            this.bt_sc.Name = "bt_sc";
+            this.bt_sc.Size = new System.Drawing.Size(159, 38);
+            this.bt_sc.TabIndex = 8;
+            this.bt_sc.Text = "辨認";
+            this.bt_sc.UseVisualStyleBackColor = true;
+            this.bt_sc.Click += new System.EventHandler(this.bt_sc_Click);
+            // 
+            // lb_show
+            // 
+            this.lb_show.AutoSize = true;
+            this.lb_show.Font = new System.Drawing.Font("新細明體", 20F);
+            this.lb_show.Location = new System.Drawing.Point(21, 444);
+            this.lb_show.Name = "lb_show";
+            this.lb_show.Size = new System.Drawing.Size(66, 27);
+            this.lb_show.TabIndex = 9;
+            this.lb_show.Text = "show";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 495);
+            this.Controls.Add(this.lb_show);
+            this.Controls.Add(this.bt_sc);
+            this.Controls.Add(this.bt_rand);
+            this.Controls.Add(this.bt_7);
+            this.Controls.Add(this.bt_6);
+            this.Controls.Add(this.bt_5);
+            this.Controls.Add(this.bt_4);
+            this.Controls.Add(this.bt_3);
+            this.Controls.Add(this.bt_2);
+            this.Controls.Add(this.bt_1);
+            this.Name = "Form1";
+            this.Text = "7 segment display";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_1;
+        private System.Windows.Forms.Button bt_2;
+        private System.Windows.Forms.Button bt_3;
+        private System.Windows.Forms.Button bt_4;
+        private System.Windows.Forms.Button bt_5;
+        private System.Windows.Forms.Button bt_6;
+        private System.Windows.Forms.Button bt_7;
+        private System.Windows.Forms.Button bt_rand;
+        private System.Windows.Forms.Button bt_sc;
+        private System.Windows.Forms.Label lb_show;
+    }
+}
+
---
+++ 7_segment_display/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_segment_display
+{
+    public partial class Form1 : Form
+    {
+
+        bool[] bt_ck = new bool[7] ;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void my_reset()
+        {
+            Control my_reset_cont = new Control();
+            for (int i = 0; i < 7; i++) {
+                bt_ck[i] = false;
+                my_reset_cont = (Button)this.Controls["bt_" + (i + 1)];
+                my_reset_cont.Text = "";
+                my_reset_cont.BackColor = Color.White;
+            }   //All buttons are unchecked
+
+            lb_show.Text = "數字：";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            my_reset();
+        }
+
+        private void bt_rand_Click(object sender, EventArgs e)
+        {
+            Random my_rand = new Random();
+            my_reset();
+            Control my_rand_cont = new Control();
+            int my_rand_ck = my_rand.Next(1, 8);
+
+            for (int i = 0; i< my_rand.Next(2, 8); i++)
+            {
+            alpaca_rand:
+                my_rand_ck = my_rand.Next(1, 8);
+                if (bt_ck[(my_rand_ck-1)] == true) { goto alpaca_rand; }
+                my_set_f(my_rand_ck);
+            }
+
+            if (my_sc_f() == -1) { lb_show.Text = "數字：非數字!"; }
+            else { lb_show.Text = "數字：" + my_sc_f().ToString(); }
+        }
+
+        private void bt_sc_Click(object sender, EventArgs e)
+        {
+            if(my_sc_f() == -1) { lb_show.Text = "數字：非數字!"; }
+            else { lb_show.Text = "數字：" + my_sc_f().ToString(); }
+        }
+
+        void my_set_f(int bt_num)
+        {
+            try
+            {
+                Control my_cont = new Control();
+                my_cont = (Button)this.Controls["bt_" + bt_num];
+
+                if(bt_ck[bt_num - 1] == true) { bt_ck[bt_num - 1] = false; my_cont.BackColor = Color.White; }
+                else { bt_ck[bt_num - 1] = true; my_cont.BackColor = Color.Black; }
+            }
+            catch { MessageBox.Show("Error!", "Error!"); }
+            
+        }
+
+        int my_sc_f()
+        {
+            int my_num = -1;
+            string my_sc="";
+
+            for (int i=0; i<7; i++)
+            {
+                if (bt_ck[i] == true) { my_sc += "1"; }
+                else { my_sc += "0"; }
+            }
+
+            switch (my_sc)
+            {
+                case "1111110":     //0
+                    my_num = 0; break;
+
+                case "0110000":     //1
+                case "0000110":
+                    my_num = 1; break;
+
+                case "1101101":     //2
+                    my_num = 2; break;
+
+                case "1111001":     //3
+                    my_num = 3; break;
+
+                case "0110011":     //4
+                    my_num = 4; break;
+
+                case "1011011":     //5
+                    my_num = 5; break;
+
+                case "1011111":     //6
+                case "0011111":
+                    my_num = 6; break;
+
+                case "1110000":     //7
+                    my_num = 7; break;
+
+                case "1111111":     //8
+                    my_num = 8; break;
+
+                case "1110011":     //9
+                case "1111011":
+                    my_num = 9; break;
+
+                default:
+                    my_num = -1; break;
+            }
+
+            return my_num;
+        }
+
+        private void bt_1_Click(object sender, EventArgs e)
+        {
+            my_set_f(1);
+        }
+
+        private void bt_2_Click(object sender, EventArgs e)
+        {
+            my_set_f(2);
+        }
+
+        private void bt_3_Click(object sender, EventArgs e)
+        {
+            my_set_f(3);
+        }
+
+        private void bt_4_Click(object sender, EventArgs e)
+        {
+            my_set_f(4);
+        }
+
+        private void bt_5_Click(object sender, EventArgs e)
+        {
+            my_set_f(5);
+        }
+
+        private void bt_6_Click(object sender, EventArgs e)
+        {
+            my_set_f(6);
+        }
+
+        private void bt_7_Click(object sender, EventArgs e)
+        {
+            my_set_f(7);
+        }
+    }
+}
